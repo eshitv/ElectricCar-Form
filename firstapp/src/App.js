5c9d8e933bd817fa9fd199e50ce09745890b92ed
eshitv <89860905+eshitv@users.noreply.github.com>
2022-06-08 10:33:05 -0700

    Multi-Step Form
    ReactJs (Context Api)

---
+++ firstapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import StepForm from './components/StepForm';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <StepForm />
+    </div>
+  );
+};
+
+export default App;
